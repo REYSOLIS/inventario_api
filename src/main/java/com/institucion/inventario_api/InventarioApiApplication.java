6b740507af0e95c4a38e5367bcadec1f81f5d8d2
reynaldo14 <reysolisavila@gmail.com>
2026-06-07 14:55:47 -0600

    commit inicial

---
+++ src/main/java/com/institucion/inventario_api/InventarioApiApplication.java
@@ -0,0 +1,14 @@
+package com.institucion.inventario_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioApiApplication {
+
+	public static void main(String[] args) {
+
+    SpringApplication.run(InventarioApiApplication.class, args);
+
+	}
+}
